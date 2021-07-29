761a4f046a713ad47aa95ab0c24b71ad380d097e
supreme <1830601430@qq.com>
2021-07-29 08:30:15 +0800

    new tradmedical

---
+++ algorithm/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+# @Time    : 2021/7/29 8:23
+# @Author  : zhibindeng
+# @Email   : zhibindeng@shu.deu.cn
+# @File    : __init__.py.py
+# @Software: PyCharm
