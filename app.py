9779f6155bbcf423e1fb938d12e58f0a2b246d87
supreme <1830601430@qq.com>
2021-07-31 15:33:53 +0800

    add app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/login',methods=['POST'])
+@app.route('/login',methods=['POST',"GET"])
 def login():
     print(request.data)
     return jsonify({'status':True})
